679cc3177b879a9b36f671698bf1b4cdc16d1c48
root <daniel.ntaye@sahanalytics.com>
2024-09-27 19:07:56 +0000

    update admin

--- database/migrations/2024_07_30_110037_create_packs_table.php
+++ database/migrations/2024_07_30_110037_create_packs_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string('name');
-            $table->string('description')->nullable();
+            $table->longText('description')->nullable();
         });
     }
 
